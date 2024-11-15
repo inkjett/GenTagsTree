20bf15fca3ee175e5bc168f96ce4c71be67692b5
DESKTOP-8B9C8M0\administrator <mail22sms@mail.ru>
2024-11-15 10:53:56 +0500

    *

--- GenTagsTree/Program.cs
+++ GenTagsTree/Program.cs
@@ -1,9 +1,10 @@
 ﻿using System.IO;
 using System.Xml;
 using System.Xml.Linq;
+//#nullable disable
 
 string fileName = "TestTree.xmlcfg";
-string path = "C:\\Users\\AutomiqUsr\\Documents\\GenTagsTree\\GenTagsTree\\bin\\Debug\\net8.0";
+string path = "C:\\Users\\administrator\\Documents\\GenTagsTree\\GenTagsTree\\bin\\Debug\\net8.0";
 string fullPath = Path.Combine(path, fileName);
 string FileData = "";
 
@@ -27,37 +28,33 @@ foreach (XmlElement node in xRoot.ChildNodes)
     if (node.Name == "Signals")
     {
         //Console.WriteLine(node.Name);
-        GetData(node);
+        Console.WriteLine( GetData(node));
     }
 }
 
 
-static void GetData(XmlElement _dataIn) // рекурсивная функция для вычитывания данных из xmlcfg
+static string  GetData(XmlNode _dataIn, string _path = "") // рекурсивная функция для вычитывания данных из xmlcfg
 {
-    if (_dataIn.Name == "Item")
+    string tmp = "";
+    for (int i = 0; i < _dataIn.ChildNodes.Count; i++)
     {
-        if (_dataIn.Attributes.GetNamedItem("Type").Value == "Folder")
+        if (_dataIn.ChildNodes[i]?.Name == "Item")
         {
-            Console.WriteLine("Folder=" + _dataIn.Attributes.GetNamedItem("Name").Value);
+            //Console.WriteLine("Name1=" + tmp); 
+            if (_dataIn.ChildNodes[i].Attributes.GetNamedItem("Type").Value == "Folder")
+            {
+                tmp += _dataIn.ChildNodes[i].Attributes.GetNamedItem("Name").Value;
+            }
+            if (_dataIn.ChildNodes[i].Attributes.GetNamedItem("Type").Value == "Double")
+            {
+                tmp += _dataIn.ChildNodes[i].Attributes.GetNamedItem("Name").Value + "\n";
+            }
+
         }
-        else if (_dataIn.Attributes.GetNamedItem("Type").Value == "Double")
+        if (_dataIn.ChildNodes[i]?.HasChildNodes ?? false)
         {
-            Console.WriteLine("Double=" + _dataIn.Attributes.GetNamedItem("Name").Value);
-        }
-        else if (_dataIn.Attributes.GetNamedItem("Type").Value == "Bool")
-        {
-            Console.WriteLine("Bool=" + _dataIn.Attributes.GetNamedItem("Name").Value);
-        }
-        else if (_dataIn.Attributes.GetNamedItem("Type").Value == "Float")
-        {
-            Console.WriteLine("Float=" + _dataIn.Attributes.GetNamedItem("Name").Value);
+             tmp += GetData(_dataIn.ChildNodes[i], tmp);
         }
     }
-    if (_dataIn.HasChildNodes)
-    {
-        foreach (XmlElement childnode in _dataIn.ChildNodes)
-        {
-            GetData(childnode);
-        }
-    }
+    return tmp;
 }
